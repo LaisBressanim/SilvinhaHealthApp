d7addb5a82e3e991c89bf0cf6142151a4cae7de5
LaisBressanim <laismartim2009@gmail.com>
2026-04-15 11:36:16 -0300

    pag 16 (de novo)

--- SilvinhaHealthApp/utils/calculos.js
+++ SilvinhaHealthApp/utils/calculos.js
@@ -12,3 +12,34 @@ valor: imc.toFixed(2),
 classificacao
 };
 }
+
+export const calcularPesoIdeal = (altura, sexo) => {
+const alturaCm = altura * 100;
+let pesoIdeal = 0;
+if (sexo === 'masculino') {
+pesoIdeal = 50 + 2.3 * ((alturaCm - 152.4) / 2.54);
+} else {
+pesoIdeal = 45.5 + 2.3 * ((alturaCm - 152.4) / 2.54);
+}
+return pesoIdeal.toFixed(1);
+};
+
+export const calcularAgua = (peso) => {
+const aguaMl = peso * 35;
+const aguaL = aguaMl / 1000;
+return {
+ml: Math.round(aguaMl),
+l: aguaL.toFixed(1)
+};
+};
+
+export const calcularTMB = (peso, altura, idade, sexo) => {
+const alturaCm = altura * 100;
+let tmb = 0;
+if (sexo === 'masculino') {
+tmb = (10 * peso) + (6.25 * alturaCm) - (5 * idade) + 5;
+} else {
+tmb = (10 * peso) + (6.25 * alturaCm) - (5 * idade) - 161;
+}
+return Math.round(tmb);
+};
